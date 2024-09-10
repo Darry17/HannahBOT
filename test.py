a95b9e718239ce163362af09586e45db947c1434
Darry Diaz <darry.a.diaz@gmail.com>
2024-09-10 12:44:32 +0800

    first test

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
-print("hello");
+import telebot
+
+API_KEY = "7239889112:AAFTAkzrbs-a_u8o29c5FvGjHci5lsV_9IQ"
+
+bot = telebot.TeleBot(API_KEY)
+
+@bot.message_handler(commands=['hi hannah'])
+def hi_hannah(message):
+    bot.reply_to(message, "Hi Mommy, Hi Daddy 😊") 
+
+bot.polling()
