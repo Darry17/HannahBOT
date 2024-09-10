bf772b2d63603c28314c27c89123caee1b272df7
Darry Diaz <darry.a.diaz@gmail.com>
2024-09-10 12:45:26 +0800

    test

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ API_KEY = "7239889112:AAFTAkzrbs-a_u8o29c5FvGjHci5lsV_9IQ"
 
 bot = telebot.TeleBot(API_KEY)
 
-@bot.message_handler(commands=['hi hannah'])
+@bot.message_handler(commands=['hi_hannah'])
 def hi_hannah(message):
     bot.reply_to(message, "Hi Mommy, Hi Daddy 😊") 
 
