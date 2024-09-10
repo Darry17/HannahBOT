32b1d18e8d9a3da62d10d8d386b61aad529f80b9
Darry Diaz <darry.a.diaz@gmail.com>
2024-09-10 13:00:43 +0800

    working

--- test.py
+++ test.py
@@ -1,15 +1,10 @@
 import telebot
-import os
 
-API_KEY = os.getenv('API_KEY')
+API_KEY = '7239889112:AAFTAkzrbs-a_u8o29c5FvGjHci5lsV_9IQ'
 bot = telebot.TeleBot(API_KEY)
 
 @bot.message_handler(commands=['hi'])
 def hi(message):
     bot.reply_to(message, "Hi Mommy, Hi Daddy 😊") 
-
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.reply_to(message, "Bot is running!")
 
 bot.polling(none_stop=True, timeout=123)
