eba000152cb1894c9eb41af6a8a3ccf76c25b60a
Darry Diaz <darry.a.diaz@gmail.com>
2024-09-10 13:46:37 +0800

    improved responses

--- hannah.py
+++ hannah.py
@@ -1,6 +1,7 @@
 import telebot
 import json
 import os
+from difflib import get_close_matches
 
 API_KEY = '7239889112:AAFTAkzrbs-a_u8o29c5FvGjHci5lsV_9IQ'
 bot = telebot.TeleBot(API_KEY)
@@ -19,12 +20,26 @@ def save_qa_data():
 
 qa_dict = load_qa_data()
 
+def find_closest_match(user_input, qa_dict):
+    # Get the list of stored questions
+    questions = list(qa_dict.keys())
+    
+    # Find the closest matches
+    closest_matches = get_close_matches(user_input, questions, n=1, cutoff=0.6)
+    
+    if closest_matches:
+        return closest_matches[0]
+    return None
+
 @bot.message_handler(func=lambda message: True)
 def handle_message(message):
     user_input = message.text.lower() 
     
-    if user_input in qa_dict:
-        bot.reply_to(message, qa_dict[user_input])
+    # Find the closest match in the database
+    closest_match = find_closest_match(user_input, qa_dict)
+    
+    if closest_match:
+        bot.reply_to(message, qa_dict[closest_match])
     else:
         msg = bot.reply_to(message, "di ko po alam sasabihin 🥺, ano po want niyong sabihin ko?")
         bot.register_next_step_handler(msg, learn_response, user_input)
